6b452cacc1ccb54cabc8a71d3bf452724a346f7c
heiko95 <39989555+heiko95@users.noreply.github.com>
2023-01-21 17:22:32 +0100

    Mapper Profile

--- BulletinBoard/Infrastructure/Profiles/EventToDomainProfile.cs
+++ BulletinBoard/Infrastructure/Profiles/EventToDomainProfile.cs
@@ -12,23 +12,33 @@ namespace hgSoftware.Infrastructure.Profiles
         {
             {
                 CreateMap<Event, PlannerEvent>()
-                    .ForMember(dest => dest.BibleText, opt => opt.MapFrom(src => !string.IsNullOrEmpty(src.Book) ? $"{src.Book} {src.Chapter}, {src.Verse}" : string.Empty))
-                    .ForMember(dest => dest.DateTime, opt => opt.MapFrom(src => new DateTime(src.Date.Year,
-                                                                                              src.Date.Month,
-                                                                                              src.Date.Day,
-                                                                                              src.Time.Hour,
-                                                                                              src.Time.Minute,
-                                                                                              src.Time.Second)))
+                    .ForMember(dest => dest.BibleText,
+                               opt => opt.MapFrom(src => !string.IsNullOrEmpty(src.Book) ? $"{src.Book} {src.Chapter}, {src.Verse}" : string.Empty))
 
-                    .ForMember(dest => dest.Date, opt => opt.Ignore())
-                    .ForMember(dest => dest.Description, opt => opt.MapFrom(src => string.Concat(src.EventDescription, " ", src.AdditionalInfo, " ", !string.IsNullOrEmpty(src.Person) ? string.Concat("durch ", src.Person) : string.Empty).Trim()))
-                    .ForMember(dest => dest.Id, opt => opt.Ignore())
-                    .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location));
+                    .ForMember(dest => dest.DateTime,
+                               opt => opt.MapFrom(src => new DateTime(src.Date.Year, src.Date.Month, src.Date.Day, src.Time.Hour, src.Time.Minute, src.Time.Second)))
+
+                    .ForMember(dest => dest.Date,
+                               opt => opt.Ignore())
+
+                    .ForMember(dest => dest.Description,
+                               opt => opt.MapFrom(src => string.Concat(src.EventDescription, " ", src.AdditionalInfo, " ", !string.IsNullOrEmpty(src.Person) ? string.Concat("durch ", src.Person) : string.Empty).Trim()))
+
+                    .ForMember(dest => dest.Id,
+                               opt => opt.Ignore())
+
+                    .ForMember(dest => dest.Location,
+                               opt => opt.MapFrom(src => src.Location));
 
                 CreateMap<Event, BibleInfo>()
-                   .ForMember(dest => dest.Book, opt => opt.MapFrom(src => src.Book!.Replace(" ", "")))
-                   .ForMember(dest => dest.Chapter, opt => opt.MapFrom(src => src.Chapter))
-                   .ForMember(dest => dest.VerseText, opt => opt.MapFrom(src => src.Verse));
+                   .ForMember(dest => dest.Book,
+                               opt => opt.MapFrom(src => src.Book!.Replace(" ", "")))
+
+                   .ForMember(dest => dest.Chapter,
+                               opt => opt.MapFrom(src => src.Chapter))
+
+                   .ForMember(dest => dest.VerseText,
+                               opt => opt.MapFrom(src => src.Verse));
             }
         }
 
